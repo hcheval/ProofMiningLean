f1fcc3f9f296a3b6da8588f69603ffadb2b109c8
alexwng <40216943+alexwng@users.noreply.github.com>
2021-10-18 11:12:44 +0300

    added intuitionistic rules and theorems

---
+++ ProofMining/IntuitionisticRules.lean
@@ -0,0 +1,40 @@
+-- Chapter 9.4 From "Articol rules"
+
+import ProofMining.Proof
+import ProofMining.Formula
+
+open Formula (falsum)
+namespace Proof
+
+theorem r44 {A B : Formula} (h : Γ ⊢ A) : Γ ⊢ B ⟹ A :=
+  let p₁ : Γ ⊢ A ⋀ B ⟹ A := weakConj' 
+  let p₂ : Γ ⊢ A ⟹ (B ⟹ A) := exportation p₁ 
+  mpon h p₂
+
+theorem r45 {A B C : Formula} (h₁ : Γ ⊢ A ⟹ B) (h₂ : Γ ⊢ A ⟹ C) : Γ ⊢ A ⟹ B ⋀ C :=
+  let p₁ : Γ ⊢ B ⋀ C ⟹ B ⋀ C := syllogism (permConj B C) (permConj C B)
+  let p₂ : Γ ⊢ B ⟹ (C ⟹ B ⋀ C) := exportation p₁
+  let p₃ : Γ ⊢ A ⟹ (C ⟹ B ⋀ C) := syllogism h₁ p₂
+  let p₄ : Γ ⊢ A ⋀ C ⟹ B ⋀ C := importation p₃
+  let p₅ : Γ ⊢ C ⋀ A ⟹ B ⋀ C := syllogism (permConj _ _) p₄
+  let p₆ : Γ ⊢ C ⟹ (A ⟹ B ⋀ C) := exportation p₅
+  let p₇ : Γ ⊢ A ⋀ A ⟹ B ⋀ C := importation (syllogism h₂ p₆)
+  syllogism (contrConj _) p₇
+
+theorem r46 {A B C : Formula} (h : Γ ⊢ A ⟹ B) : Γ ⊢ A ⋀ C ⟹ B ⋀ C :=
+  let p₁ : Γ ⊢ C ⋀ A ⟹ B := importation (r44 h)
+  let p₂ : Γ ⊢ A ⋀ C ⟹ B := syllogism (permConj _ _) p₁
+  let p₃ : Γ ⊢ A ⋀ C ⟹ C := syllogism (permConj _ _) (weakConj _ _)
+  r45 p₂ p₃
+
+theorem r49 {A B C : Formula} (h₁ : Γ ⊢ A ⟹ C) (h₂ : Γ ⊢ B ⟹ C) : Γ ⊢ A ⋁ B ⟹ C :=
+  let p₁ : Γ ⊢ A ⋁ B ⟹ A ⋁ C := expansion A h₂
+  let p₂ : Γ ⊢ C ⋁ A ⟹ C ⋁ C := expansion C h₁
+  let p₃ : Γ ⊢ A ⋁ C ⟹ C ⋁ C := syllogism (permDisj _ _) p₂
+  let p₄ : Γ ⊢ A ⋁ C ⟹ C := syllogism p₃ (contrDisj C)
+  syllogism p₁ p₄
+
+theorem r50 {A B : Formula} (h₁ : Γ ⊢ A) (h₂ : Γ ⊢ B) : Γ ⊢ A ⋀ B :=
+  let p₁ : Γ ⊢ A ⟹ (B ⟹ A ⋀ B) := exportation (syllogism (permConj A B) (permConj B A))
+  let p₂ : Γ ⊢ B ⟹ A ⋀ B  := mpon h₁ p₁
+  mpon h₂ p₂
---
+++ ProofMining/IntuitionisticTheorems.lean
@@ -0,0 +1,110 @@
+-- Chapter 9.3 From "Articol metateoreme"
+
+import ProofMining.Proof
+import ProofMining.Formula
+import ProofMining.IntuitionisticRules
+
+open Formula (falsum)
+namespace Proof
+
+theorem t19 (A B : Formula) : Γ ⊢ (A ⋀ B ⟹ B) := 
+  let p₁ : Γ ⊢ (A ⋀ B ⟹ B ⋀ A) := permConj _ _
+  let p₂ : Γ ⊢ (B ⋀ A ⟹ B) := weakConj _ _
+  syllogism p₁ p₂
+
+theorem t20 (A B : Formula) : Γ ⊢ (B ⟹ A ⋁ B) := 
+  let p₁ : Γ ⊢ (B ⟹ B ⋁ A) := weakDisj _ _
+  let p₂ : Γ ⊢ (B ⋁ A ⟹ A ⋁ B) := permDisj _ _
+  syllogism p₁ p₂
+
+theorem t21 (A : Formula) : Γ ⊢ A ⟹ A :=
+  let p₁ : Γ ⊢ A ⟹ A ⋀ A := contrConj _ 
+  let p₂ : Γ ⊢ A ⋀ A ⟹ A := weakConj _ _
+  syllogism p₁ p₂
+
+theorem t18 (A B : Formula) : Γ ⊢ ((A ⟹ falsum) ⋀ A ⟹ B) :=
+  let p₁ : Γ ⊢ (falsum ⟹ B) := exFalso _
+  let p₂ : Γ ⊢ ((A ⟹ falsum) ⋀ A ⟹ falsum) := importation (t21 (A ⟹ falsum))
+  syllogism p₂ p₁
+
+theorem t22 (A B : Formula) : Γ ⊢ A ⟹ (B ⟹ (A ⋀ B)) :=
+  let p₁ : Γ ⊢ (A ⋀ B) ⟹ (A ⋀ B) := t21 _
+  exportation p₁
+
+theorem t23 (A B : Formula) : Γ ⊢ A ⟹ (B ⟹ A) :=
+  let p₁ : Γ ⊢ (A ⋀ B) ⟹ A := weakConj _ _
+  exportation p₁
+
+theorem t24 (A : Formula) : Γ ⊢ (A ⟹ ((A ⟹ falsum) ⟹ falsum)) :=
+  let p₁ : Γ ⊢ (A ⋀ (A ⟹ falsum) ⟹ (A ⟹ falsum) ⋀ A) := permConj _ _
+  let p₂ : Γ ⊢ (A ⋀ (A ⟹ falsum) ⟹ falsum) := syllogism p₁ (t18 A falsum)
+  exportation p₂
+
+theorem t25 (A : Formula) : Γ ⊢ ((A ⋁ (A ⟹ falsum)) ⟹ falsum) ⟹ falsum := 
+  sorry
+
+theorem t26a (A : Formula) : Γ ⊢ (A ⟹ falsum) ⟹ (((A ⟹ falsum) ⟹ falsum) ⟹ falsum) :=
+  t24 (A ⟹ falsum)
+
+theorem t26b (A : Formula) : Γ ⊢ (((A ⟹ falsum) ⟹ falsum) ⟹ falsum) ⟹ (A ⟹ falsum) :=
+  sorry
+
+theorem t27a (A B : Formula) : Γ ⊢ (((A ⟹ B) ⟹ falsum) ⟹ falsum) ⟹ (A ⟹ ((B ⟹ falsum) ⟹ falsum)) :=
+  sorry
+
+theorem t27b (A B : Formula) : Γ ⊢ (A ⟹ ((B ⟹ falsum) ⟹ falsum)) ⟹ (((A ⟹ B) ⟹ falsum) ⟹ falsum) :=
+  sorry
+
+theorem t28a (A B : Formula) : Γ ⊢ (((A ⟹ B) ⟹ falsum) ⟹ falsum) ⟹ (((A ⟹ falsum) ⟹ falsum) ⟹ ((B ⟹ falsum) ⟹ falsum)) :=
+  sorry
+
+theorem t28b (A B : Formula) : Γ ⊢ (((A ⟹ falsum) ⟹ falsum) ⟹ ((B ⟹ falsum) ⟹ falsum)) ⟹ (((A ⟹ B) ⟹ falsum) ⟹ falsum) :=
+  sorry
+
+theorem t29a (A B : Formula) : Γ ⊢ (A ⟹ ((B ⟹ falsum) ⟹ falsum)) ⟹ (((A ⟹ falsum) ⟹ falsum) ⟹ (B ⟹ falsum)) :=
+  sorry
+
+theorem t29b (A B : Formula) : Γ ⊢ (((A ⟹ falsum) ⟹ falsum) ⟹ (B ⟹ falsum)) ⟹ (A ⟹ ((B ⟹ falsum) ⟹ falsum)) :=
+  sorry
+
+theorem t30a (A B : Formula) : Γ ⊢ (((A ⋀ B) ⟹ falsum) ⟹ falsum) ⟹ (((A ⟹ falsum) ⟹ falsum) ⋀ ((B ⟹ falsum) ⟹ falsum)) :=
+  sorry
+
+theorem t30b (A B : Formula) : Γ ⊢ (((A ⟹ falsum) ⟹ falsum) ⋀ ((B ⟹ falsum) ⟹ falsum)) ⟹ (((A ⋀ B) ⟹ falsum) ⟹ falsum) :=
+  sorry
+
+theorem t31 (A B : Formula) : Γ ⊢ (((A ⋁ B) ⟹ falsum) ⟹ falsum) ⟹ ((A ⟹ falsum) ⟹ falsum) ⟹ ((B ⟹ falsum) ⟹ falsum) :=
+  sorry
+
+theorem t32a (A B : Formula) : Γ ⊢ (((A ⋁ B) ⟹ falsum) ⟹ falsum) ⟹ (((A ⟹ falsum) ⋀ (B ⟹ falsum)) ⟹ falsum) :=
+  sorry
+
+theorem t32b (A B : Formula) : Γ ⊢ (((A ⟹ falsum) ⋀ (B ⟹ falsum)) ⟹ falsum) ⟹ (((A ⋁ B) ⟹ falsum) ⟹ falsum) :=
+  sorry
+
+theorem t33a (A B : Formula) : Γ ⊢ ((A ⋀ B) ⟹ falsum) ⟹ A ⟹ (B ⟹ falsum) :=
+  let p₁ : Γ ⊢ (A ⋀ B) ⟹ ((A ⋀ B) ⟹ falsum)  ⟹ falsum := t24 _
+  sorry
+
+theorem t33b (A B : Formula) : Γ ⊢ (A ⟹ (B ⟹ falsum)) ⟹ ((A ⋀ B) ⟹ falsum) :=
+  sorry
+
+theorem t35 (A B : Formula) : Γ ⊢ A ⋁ B ⟹ (A ⟹ falsum) ⟹ B :=
+  let p₁ : Γ ⊢ (A ⟹ falsum) ⋀ A ⟹ B := t18 _ _
+  let p₂ : Γ ⊢ A ⟹ (A ⟹ falsum) ⟹ B := exportation (syllogism (permConj _ _) p₁)
+  let p₃ : Γ ⊢ B ⟹ (A ⟹ falsum) ⟹ B := t23 _ _
+  r49 p₂ p₃
+
+theorem t40a (A B : Formula) : Γ ⊢ ((A ⋁ B) ⟹ falsum) ⟹ ((A ⟹ falsum) ⋀ (B ⟹ falsum)) :=
+  let p₃ : Γ ⊢ A ⟹ (((A ⋁ B) ⟹ falsum) ⟹ falsum) := syllogism (weakDisj A B) (t24 (A ⋁ B))
+  let p₄ : Γ ⊢ ((A ⋁ B) ⟹ falsum) ⋀ A ⟹ falsum := syllogism (permConj ((A ⋁ B) ⟹ falsum) A) (importation p₃)
+  let p₁ : Γ ⊢ ((A ⋁ B) ⟹ falsum) ⟹ (A ⟹ falsum) := exportation p₄
+
+  let p₅ : Γ ⊢ B ⟹ (((A ⋁ B) ⟹ falsum) ⟹ falsum) := syllogism (t20 A B) (t24 (A ⋁ B))
+  let p₆ : Γ ⊢ ((A ⋁ B) ⟹ falsum) ⋀ B ⟹ falsum := syllogism (permConj ((A ⋁ B) ⟹ falsum) B) (importation p₅)
+  let p₂ : Γ ⊢ ((A ⋁ B) ⟹ falsum) ⟹ (B ⟹ falsum) := exportation p₆
+
+  r45 p₁ p₂
+
+theorem t40b (A B : Formula) : Γ ⊢ ((A ⟹ falsum) ⋀ (B ⟹ falsum)) ⟹ ((A ⋁ B) ⟹ falsum) :=
+  sorry
