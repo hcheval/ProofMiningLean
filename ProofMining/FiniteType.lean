93d0bb0980c005bfc4d969ea5a01b5d9fad39ec4
alex weng <40216943+alexwng@users.noreply.github.com>
2021-10-20 19:26:37 +0300

    Update FiniteType.lean

--- ProofMining/FiniteType.lean
+++ ProofMining/FiniteType.lean
@@ -11,7 +11,14 @@ def deg : FiniteType -> Nat
   | FiniteType.zero => 0
   | FiniteType.application ρ τ => Nat.max (deg τ) ((deg ρ) + 1)
 
-
-#eval FiniteType.deg (FiniteType.zero ↣ FiniteType.zero ↣ (FiniteType.zero ↣ FiniteType.zero))
+#eval FiniteType.deg (FiniteType.zero ↣ FiniteType.zero ↣ FiniteType.zero ↣ FiniteType.zero)
 
 end FiniteType
+
+inductive PureFiniteType where
+| zero : PureFiniteType
+| application : PureFiniteType → PureFiniteType
+
+def transfrom : PureFiniteType → FiniteType
+ | PureFiniteType.zero => FiniteType.zero
+ | PureFiniteType.application ρ => (transfrom ρ) ↣ FiniteType.zero
