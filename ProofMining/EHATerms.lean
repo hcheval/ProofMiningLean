8d1b7fc7a1a09d02b62fb5919ef931208c7f22dc
alex weng <40216943+alexwng@users.noreply.github.com>
2021-10-27 17:31:33 +0300

    Update EHATerms.lean

--- ProofMining/EHATerms.lean
+++ ProofMining/EHATerms.lean
@@ -2,7 +2,22 @@
 
 import ProofMining.FiniteType
 
-inductive EHATerms : FiniteType → Type where
-  | const : EHATerms ρ
-  | var : EHATerms ρ
-  | app : (τ : FiniteType) → EHATerms (τ ↣ ρ) → EHATerms τ → EHATerms ρ
+-- inductive EHATerms (ρ: FiniteType) where
+--   | const : EHATerms ρ
+--   | var : EHATerms ρ
+--   | app : {τ : FiniteType} → EHATerms (τ ↣ ρ) → EHATerms τ → EHATerms ρ
+
+
+inductive EHATerms: FiniteType → Type where
+  | const : {ρ: FiniteType} → EHATerms ρ
+  | var : {ρ: FiniteType} → EHATerms ρ
+  | app : {ρ: FiniteType} → {τ : FiniteType} → EHATerms (τ ↣ ρ) → EHATerms τ → EHATerms ρ
+
+
+variable (ρ : FiniteType) (τ : FiniteType)
+
+#check EHATerms ρ
+#check EHATerms.const
+#check EHATerms.var
+#check EHATerms (τ ↣ ρ)
+#check EHATerms.app
